04feb9e9b58baec306d7c28f49ddf48993c971d9
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-04-10 17:53:10 +0530

    add the monior url

--- Uniwatch/urls.py
+++ Uniwatch/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include  # include ko import karna mat bhoolna
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('monitor.urls')),  # Ab humari API '/api/add-server/' par available hogi
+    path('', include('monitor.urls')),
 ]
